4d58f96b23de859c54732aa29382f84ede6599ab
Yehya Dhaini <Yehya Dhaini>
2024-11-01 21:38:25 +0200

    drivers menu

--- Delivery Company.py
+++ Delivery Company.py
@@ -21,4 +21,21 @@ class Delivery_system(self):
             else:
                 print("Invalid choice. Please try again.")
     
-    
+    def drivers_menu(self):
+        while True:
+            print("Enter:")
+            print("1. To view all the drivers")
+            print("2. To add a driver")
+            print("3. Check similar drivers")
+            print("4. To go back to the main menu")
+            choice = input("Enter your choice: ")
+            if choice == 1:
+                self.view_drivers()
+            elif choice == 2:
+                self.add_drivers()
+            elif choice == 3:
+                self.check_similar_drivers()
+            elif choice == 4:
+                break
+            else:
+                print("Invalid choice. Please try again.")
